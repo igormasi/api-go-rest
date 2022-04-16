3c75607ae0e6197e368a367657fd8bee45b7ab98
Igor M Silva <78515731+igormasi@users.noreply.github.com>
2022-04-16 08:38:54 -0400

    Initial commit

---
+++ routes/routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/igormasi/go-rest-api/controllers"
+)
+
+func HandleRequest() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/personalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
